252be0f374139cdfc152e2bc1b9024cb8d8bc2b8
qizhengyaoyao <28369996+qizhengyaoyao@users.noreply.github.com>
2021-05-20 22:34:22 +1000

    html page structure revised.

--- app.py
+++ app.py
@@ -62,9 +62,9 @@ def main():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/model")
+def model():
+    return render_template("model.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
