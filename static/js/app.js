52332105f428a6c5cd15b9d73cbf56af1a88b0b2
unknown <welan.c.13+gh@gmail.com>
2021-06-05 00:05:42 +1000

    changed Companies to drop-down menu selection

--- static/js/app.js
+++ static/js/app.js
@@ -1,56 +1,71 @@
-(function ($, document, window) {
+// (function ($, document, window) {
 
-	$(document).ready(function () {
+// 	$(document).ready(function () {
 
-		function setCountDown() {
+// 		function setCountDown() {
 
-			// set the date we're counting down to
-			var target_date = new Date($(".counter-wrap").data("date-target")).getTime();
+// 			// set the date we're counting down to
+// 			var target_date = new Date($(".counter-wrap").data("date-target")).getTime();
 
-			// variables for time units
-			var days, hours, minutes, seconds;
+// 			// variables for time units
+// 			var days, hours, minutes, seconds;
 
-			// update the tag with id "countdown" every 1 second
-			setInterval(function () {
+// 			// update the tag with id "countdown" every 1 second
+// 			setInterval(function () {
 
-				// find the amount of "seconds" between now and target
-				var current_date = new Date().getTime();
-				var seconds_left = (target_date - current_date) / 1000;
+// 				// find the amount of "seconds" between now and target
+// 				var current_date = new Date().getTime();
+// 				var seconds_left = (target_date - current_date) / 1000;
 
-				if (current_date < target_date) {
-					// do some time calculations
-					days = (parseInt(seconds_left / 86400) < 10) ? "0" + parseInt(seconds_left / 86400) : parseInt(seconds_left / 86400);
-					seconds_left = seconds_left % 86400;
+// 				if (current_date < target_date) {
+// 					// do some time calculations
+// 					days = (parseInt(seconds_left / 86400) < 10) ? "0" + parseInt(seconds_left / 86400) : parseInt(seconds_left / 86400);
+// 					seconds_left = seconds_left % 86400;
 
-					hours = (parseInt(seconds_left / 3600) < 10) ? "0" + parseInt(seconds_left / 86400) : parseInt(seconds_left / 86400);
-					seconds_left = seconds_left % 3600;
+// 					hours = (parseInt(seconds_left / 3600) < 10) ? "0" + parseInt(seconds_left / 86400) : parseInt(seconds_left / 86400);
+// 					seconds_left = seconds_left % 3600;
 
-					minutes = (parseInt(seconds_left / 60) < 10) ? "0" + parseInt(seconds_left / 60) : parseInt(seconds_left / 60);
-					seconds = (parseInt(seconds_left % 60) < 10) ? "0" + parseInt(seconds_left % 60) : parseInt(seconds_left % 60);
-				} else {
-					days = '00';
-					hours = '00';
-					minutes = '00';
-					seconds = '00';
-				}
-				// format countdown string + set tag value
+// 					minutes = (parseInt(seconds_left / 60) < 10) ? "0" + parseInt(seconds_left / 60) : parseInt(seconds_left / 60);
+// 					seconds = (parseInt(seconds_left % 60) < 10) ? "0" + parseInt(seconds_left % 60) : parseInt(seconds_left % 60);
+// 				} else {
+// 					days = '00';
+// 					hours = '00';
+// 					minutes = '00';
+// 					seconds = '00';
+// 				}
+// 				// format countdown string + set tag value
 
-				$(".days .number").html(days);
-				$(".hours .number").html(hours);
-				$(".minutes .number").html(minutes);
-				$(".seconds .number").html(seconds);
+// 				$(".days .number").html(days);
+// 				$(".hours .number").html(hours);
+// 				$(".minutes .number").html(minutes);
+// 				$(".seconds .number").html(seconds);
 
-			}, 1000);
+// 			}, 1000);
 
-		}
+// 		}
 
-		setCountDown();
+// 		setCountDown();
 
-		new WOW().init();
-	});
+// 		new WOW().init();
+// 	});
 
-	$(window).load(function () {
+// 	$(window).load(function () {
 
-	});
+// 	});
 
-})(jQuery, document, window);
+// })(jQuery, document, window);
+
+// from data.js
+var tableData = data;
+
+var selectCompany = d3.select("#company");
+
+// Get the companies from the data
+var companiesMapped = tableData.map(companies => companies.company);
+
+var companyPlaceHolder = selectCompany.append("option").html('<i class="fa fa - building - o"></i>Company');
+
+companiesMapped.forEach((comp) => {
+	selectCompany.append("option").text(comp);
+});
+
