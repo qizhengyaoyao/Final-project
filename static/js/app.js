5afafd3de9935b410bc123091c5f3020f8c134df
unknown <welan.c.13+gh@gmail.com>
2021-06-14 21:54:26 +1000

    features/ form inputs now autofill after every prediction

--- static/js/app.js
+++ static/js/app.js
@@ -69,3 +69,40 @@ companiesMapped.forEach((comp) => {
 	selectCompany.append("option").text(comp);
 });
 
+// get the last set of inputs and refill into form
+try {
+	// retrieve the last set of inputs
+	var inputMovie = d3.select("#input_movie").text();
+	var inputPopularity = d3.select("#input_popularity").text().replace(/,/g, "");
+	var inputBudget = d3.select("#input_budget").text().replace(/,/g, "").replace("$", "");
+	var inputDay = d3.select("#input_day").text();
+	var inputYear = d3.select("#input_year").text();
+	var inputLangauge = d3.select("#input_language").text();
+	var inputDuration = d3.select("#input_duration").text();
+	var inputCountry = d3.select("#input_country").text();
+	var inputGenre = d3.select("#input_genre").text();
+	var inputCompany = d3.select("#input_company").text();
+	var inputDirector = d3.select("#input_director").text();
+	var inputWriter = d3.select("#input_writer").text();
+	var inputActor = d3.select("#input_actor").text();
+
+	// refill the input fields with the previous inputs
+	d3.select("#moviename").attr("value", inputMovie);
+	d3.select("#popularity").attr("value", inputPopularity);
+	d3.select("#budget").attr("value", inputBudget);
+	d3.select("#day").attr("value", inputDay);
+	d3.select("#year").attr("value", inputYear);
+	d3.select("#language").attr("value", inputLangauge);
+	d3.select("#duration").attr("value", inputDuration);
+	d3.select("#country").attr("value", inputCountry);
+	d3.select("#genre").attr("value", inputGenre);
+	d3.select("#company").attr("value", inputCompany);
+	d3.select("#director").attr("value", inputDirector);
+	d3.select("#writer").attr("value", inputWriter);
+	d3.select("#actor").attr("value", inputActor);
+
+
+
+} catch (err) { // catch any errors
+	console.log("Not enough values defined!");
+};
